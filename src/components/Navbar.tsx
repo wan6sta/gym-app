5d20f3589b0997fb878df5e7b4366d1242aae0c9
Korablin Ivan <wan6sta@gmail.com>
2022-07-14 22:19:05 +0300

    first

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import {Stack} from '@mui/material'
+
+import Logo from '../assets/images/Logo.png'
+
+const Navbar = () => {
+  return <div>
+      <Stack>
+
+      </Stack>
+    </div>
+}
+
+export default Navbar;
